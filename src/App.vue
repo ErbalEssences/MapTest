981a725b94bbef1d053b9287c8ce1a73d33683c7
Alexandria Erb <49882103+ErbalEssences@users.noreply.github.com>
2020-09-17 13:02:19 -0500

    commit to add missing files

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <World />
+  </div>
+</template>
+
+<script>
+import World from './components/World.vue'
+
+export default {
+  name: 'App',
+  components: {
+    World
+  }
+}
+</script>
+
+<style scoped lang="scss">
+
+$background-red: #ae0001;
+$dark-red: #7e0607;
+$land-red: #960202;
+
+
+
+#app {
+    background-color: $background-red;
+    min-height: 100%;
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    left: 0;
+    top: 0;
+    z-index: 10;
+}
+</style>
+
+
